6e15be676cbc86f1883dc4bb70cbf526744f580c
Muzammil Ahmad Karimi <72164898+muzammilkarimi@users.noreply.github.com>
2025-02-01 14:49:48 +0530

    [3151] [Time Beats: 100.00%] [Memory Beats: 2.79%] - LeetPush

--- DCP-02-25/3151-Special-Array-I.cpp
+++ DCP-02-25/3151-Special-Array-I.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
         for (int i = 0; i < nums.size() - 1; i++) {
-            int f = nums[i] % 2, s = nums[i + 1] % 2;
-            if(f==s) return false;
+            if(nums[i] % 2==nums[i + 1] % 2) return false;
         }
         return true;
     }
