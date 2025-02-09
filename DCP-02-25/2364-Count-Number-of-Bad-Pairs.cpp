5291ab9554ef6291ed95c21519a79bde84658c3f
Muzammil Ahmad Karimi <72164898+muzammilkarimi@users.noreply.github.com>
2025-02-09 15:55:45 +0530

    [2364] [Time Beats: 99.24%] [Memory Beats: 98.94%] - LeetPush

--- DCP-02-25/2364-Count-Number-of-Bad-Pairs.cpp
+++ DCP-02-25/2364-Count-Number-of-Bad-Pairs.cpp
@@ -2,15 +2,29 @@ class Solution {
 public:
     long long countBadPairs(vector<int>& nums) {
         int n=nums.size();
-        unordered_map<int,int> freq;
-        long long goodpairs=0;
-        for(int i=0; i<n; i++){
-            if(freq.count(nums[i]-i)!=0)
-                goodpairs+=freq[nums[i]-i];
-            freq[nums[i]-i]++;
+        long long totalpairs = n*(n-1LL)/2;
+        for(int i=0; i<n; i++)
+            nums[i]-=i;
+        sort(nums.begin(),nums.end());
+        long long goodpairs=1;
+        for(int i=1; i<n; i++){
+            if(nums[i]==nums[i-1]) goodpairs++;
+            else{
+                totalpairs-=goodpairs*(goodpairs-1)/2;
+                goodpairs=1;
+            }
         }
-        long long badpairs = n*(n-1LL)/2 - goodpairs;
-        return badpairs;
+        totalpairs-=goodpairs*(goodpairs-1)/2;
+        return totalpairs;
+        // unordered_map<int,int> freq;
+        // long long goodpairs=0;
+        // for(int i=0; i<n; i++){
+        //     if(freq.count(nums[i]-i)!=0)
+        //         goodpairs+=freq[nums[i]-i];
+        //     freq[nums[i]-i]++;
+        // }
+        // long long badpairs = n*(n-1LL)/2 - goodpairs;
+        // return badpairs;
 
         
     }
