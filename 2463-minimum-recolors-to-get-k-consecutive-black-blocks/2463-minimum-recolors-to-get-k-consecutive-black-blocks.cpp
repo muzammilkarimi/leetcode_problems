f675e69d54f63c2afbd656e9e4c387d453b6bc3d
Muzammil Ahmad Karimi <72164898+muzammilkarimi@users.noreply.github.com>
2025-03-08 22:14:36 +0530

    Time: 8 ms (3.76%), Space: 8.5 MB (8.46%) - LeetHub

---
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        int count=0,ans=101,left=0,right=0,n=blocks.size();
+        while(right<n){
+            if((right-left)<k){
+                if(blocks[right]=='W') count++;
+                right++;
+            }
+            else{
+                cout<<count<<" ";
+                ans=min(ans,count);
+                if(blocks[left]=='W') count--;
+                left++;
+                if(blocks[right]=='W') count++;
+                right++;
+                if(count==0)return 0;
+            }
+        }
+        return min(count,ans);
+
+        
+    }
+};
