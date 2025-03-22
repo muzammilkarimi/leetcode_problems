6ee871bdc0cadf3a00f4e5442b529a1e201e49bf
Muzammil Ahmad Karimi <72164898+muzammilkarimi@users.noreply.github.com>
2025-03-22 15:32:41 +0530

    Time: 39 ms (75.17%), Space: 127.9 MB (83.7%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    void dfs(int node,vector<int>&comp,vector<vector<int>>&adj_list,vector<bool>&vis){
+    void dfs(int node,int&ver,int&edg,vector<vector<int>>&adj_list,vector<bool>&vis){
         vis[node]=true;
-        comp.push_back(node);
+        ver++;
         for(int n:adj_list[node]){
-            if(!vis[n]) dfs(n,comp,adj_list,vis);
+            edg++;
+            if(!vis[n]) dfs(n,ver,edg,adj_list,vis);
         }
     }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
@@ -18,16 +19,9 @@ public:
         int nodeincomp=1;
         for(int i=0; i<n; i++){
             if(!vis[i]){
-                vector<int> comp;
-                dfs(i,comp,adj_list,vis);
-                bool iscompl=true;
-                for(auto &it:comp){
-                    if(adj_list[it].size()!=comp.size() - 1){
-                        iscompl=false;
-                        break;
-                    }
-                }
-                if(iscompl) ans++;
+                int ver=0,edg=0;
+                dfs(i,ver,edg,adj_list,vis);
+                if(edg==ver*(ver-1)) ans++;
                 
             }
         }
