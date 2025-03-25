8a1c184603ff43fb73bd4a5e281df35c8d9ce1be
Muzammil Ahmad Karimi <72164898+muzammilkarimi@users.noreply.github.com>
2025-03-26 00:56:44 +0530

    Time: 1183 ms (5.04%), Space: 218.4 MB (62.58%) - LeetHub

---
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
+        map<int, int> vert, hor;
+        for(auto &x: rectangles) {
+            vert[x[1]+1] += 1;
+            vert[x[3]] -= 1;
+        }
+        int curr = 0;
+        int ans = 0;
+        for(auto &x: vert) {
+            curr += x.second;
+            if(curr == 0)
+                ans++;
+        }
+        --ans;
+        if(ans>=2)
+            return true;
+        
+        ans = 0;
+        curr = 0;
+        for(auto &x: rectangles) {
+            hor[x[0]+1] += 1;
+            hor[x[2]] -= 1;
+        }
+        for(auto x: hor)
+            cout<<x.first<<" "<<x.second<<"\n";
+        for(auto &x: hor) {
+            curr += x.second;
+            if(curr == 0)
+                ans++;
+        }
+        --ans;
+        if(ans>=2)
+            return true;
+        return false;
+    }
+};
