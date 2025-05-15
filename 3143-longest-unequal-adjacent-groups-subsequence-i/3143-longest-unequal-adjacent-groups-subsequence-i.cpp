03181bf6e236e0362e1127ef03e7272230582c22
Muzammil Ahmad Karimi <72164898+muzammilkarimi@users.noreply.github.com>
2025-05-15 16:05:47 +0530

    Time: 0 ms (100%), Space: 30 MB (40.56%) - LeetHub

--- 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     vector<string> getLongestSubsequence(vector<string>& words,vector<int>& groups) {
         
-        for (int i = 0; i < words.size() - 1;) {
-            if (groups[i] == groups[i + 1]) {
-                words.erase(words.begin() + i);
-                groups.erase(groups.begin() + i);
-            } else
-                i++;
-        }
-        return words;
-        //     vector<string> ans;
-        //     vector<int> gr;
-        //     ans.push_back(words[0]);
-        //     gr.push_back(groups[0]);
-        //     int count=1;
-        //     for(int i=1; i<words.size(); i++){
-        //         if(groups[i]!=gr[count-1]){
-        //             ans.push_back(words[i]);
-        //             gr.push_back(groups[i]);
-        //             count++;
-        //         }
-        //     }
-        //     return ans;
+        // for (int i = 0; i < words.size() - 1;) {
+        //     if (groups[i] == groups[i + 1]) {
+        //         words.erase(words.begin() + i);
+        //         groups.erase(groups.begin() + i);
+        //     } else
+        //         i++;
+        // }
+        // return words;
+            vector<string> ans;
+            vector<int> gr;
+            ans.push_back(words[0]);
+            gr.push_back(groups[0]);
+            int count=1;
+            for(int i=1; i<words.size(); i++){
+                if(groups[i]!=gr[count-1]){
+                    ans.push_back(words[i]);
+                    gr.push_back(groups[i]);
+                    count++;
+                }
+            }
+            return ans;
         }
     };
