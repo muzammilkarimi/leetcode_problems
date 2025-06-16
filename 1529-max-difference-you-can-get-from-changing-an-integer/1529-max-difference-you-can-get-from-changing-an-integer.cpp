14867a72507275265fb57e8e47b1dd17bffb9e03
Muzammil Ahmad Karimi <72164898+muzammilkarimi@users.noreply.github.com>
2025-06-16 11:06:36 +0530

    Time: 0 ms (100%), Space: 9.2 MB (13.52%) - LeetHub

--- 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.cpp
@@ -1,34 +1,28 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     int maxDiff(int num) {
-        string str1 = to_string(num);
-        string str2 = str1;
-        int n = str1.size();
-        // Get max number
-        int i;
-        for(i = 0; i < n; i++) {
-            if(str1[i] != '9') break;
-        }
-        char ele1 = str1[i];
-        for(int k = 0; k < n; k++) {
-            if(str1[k] == ele1) str1[k] = '9';
-        }
-        // Get min number
-        char ele2 = str2[0];
-        char replace = '1';
-        if(ele2 == '1') {
-            for(int k = 1; k < n; k++) {
-                if(str2[k] != '0' && str2[k] != '1') {
-                    ele2 = str2[k];
-                    replace = '0';
-                    break;
-                }
+        string numStr = to_string(num);
+        set<char> uniqueDigits(numStr.begin(), numStr.end());
+        int maxVal = num, minVal = num;
+
+        for (char digit : uniqueDigits) {
+            for (char newDigit = '0'; newDigit <= '9'; ++newDigit) {
+                if (numStr[0] == digit && newDigit == '0') continue;
+                string newNumStr = numStr;
+                replace(newNumStr.begin(), newNumStr.end(), digit, newDigit);
+                int newNum = stoi(newNumStr);
+                maxVal = max(maxVal, newNum);
+                minVal = min(minVal, newNum);
             }
         }
-        for(int k = 0; k < n; k++) {
-            if(str2[k] == ele2) str2[k] = replace;
-        }
 
-        return stoi(str1) - stoi(str2);
+        return maxVal - minVal;
     }
 };
