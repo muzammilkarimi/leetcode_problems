9c7b70330f18a386b322675b4c20abb2ed5953f8
Muzammil Ahmad Karimi <72164898+muzammilkarimi@users.noreply.github.com>
2025-07-18 00:25:20 +0530

    Time: 123 ms (36.28%), Space: 128.9 MB (46.9%) - LeetHub

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/3491-find-the-maximum-length-of-valid-subsequence-ii.cpp
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/3491-find-the-maximum-length-of-valid-subsequence-ii.cpp
@@ -7,7 +7,7 @@ public:
         int maxLength = 2;
 
         for (int targetMod = 0; targetMod < k; ++targetMod) {
-            std::vector<int> remainderDP(k, 0);
+            vector<int> remainderDP(k, 0);
 
             for (int num : nums) {
                 int numMod = num % k;
@@ -16,7 +16,7 @@ public:
             }
 
             for (int length : remainderDP) {
-                maxLength = std::max(maxLength, length);
+                maxLength = max(maxLength, length);
             }
         }
 
