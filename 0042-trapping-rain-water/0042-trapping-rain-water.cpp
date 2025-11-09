e637ab760c334e4abdc64afdc88e80fafbaf1cc7
Muzammil Ahmad Karimi <72164898+muzammilkarimi@users.noreply.github.com>
2025-11-10 02:24:41 +0530

    Time: 0 ms (100%), Space: 27.2 MB (30.85%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int trap(vector<int>& arr) {
-        int n = arr.size(), res = 0;
-        vector<int> lmax(n), rmax(n);
-        lmax[0] = arr[0];
-        rmax[n - 1] = arr[n - 1];
-        for (int i = 1; i < n; i++)
-            lmax[i] = max(arr[i], lmax[i - 1]);
-        for (int i = n - 2; i >= 0; i--)
-            rmax[i] = max(arr[i], rmax[i + 1]);
-        for (int i = 1; i < n - 1; i++)
-            res = res + (min(lmax[i],rmax[i]) - arr[i]);
-        return res;
+        int n = arr.size();
+        vector<int> lmax(n),rmax(n);
+        lmax[0]=arr[0];
+        rmax[n-1]=arr[n-1];
+        for(int i=1; i<arr.size(); i++){
+            lmax[i]=max(arr[i],lmax[i-1]);
+        }
+        for(int i=arr.size()-2; i>=0; i--){
+            rmax[i]=max(arr[i],rmax[i+1]);
+        }
+        int ans=0;
+        for(int i=0; i<arr.size(); i++){
+            ans+=min(rmax[i],lmax[i])-arr[i];
+        }
+        return ans;
+        
     }
 };
