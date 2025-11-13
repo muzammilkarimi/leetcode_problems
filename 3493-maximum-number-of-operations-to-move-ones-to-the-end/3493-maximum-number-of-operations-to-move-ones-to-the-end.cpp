b083129475f7801bdea22e2744f775c0c4680d44
Muzammil Ahmad Karimi <72164898+muzammilkarimi@users.noreply.github.com>
2025-11-14 02:12:59 +0530

    Time: 9 ms (22.26%), Space: 15.7 MB (75.18%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -3,12 +3,8 @@ public:
     int maxOperations(string s) {
         int one=0,ans=0;
         for(int i=0; i<s.size(); i++){
-            if(s[i]=='0'){
-                while(i+1<s.size() and s[i+1]=='0') i++;
-                ans+=one;
-
-            }
-            else one++;
+            if(s[i]=='1') one++;
+            else if(i >0 && s[i-1]=='1') ans+=one;
         }
         return ans;
 
