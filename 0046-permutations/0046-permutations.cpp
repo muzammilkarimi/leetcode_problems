f7753abe162a58526ed99baedc3bbb16f2e7392d
Muzammil Ahmad Karimi <72164898+muzammilkarimi@users.noreply.github.com>
2025-12-22 03:52:46 +0530

    Time: 0 ms (100%), Space: 10.4 MB (97.63%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    void solve(vector<int>& nums,vector<vector<int>>&ans,vector<int> &curr, int idx){
+    void solve(vector<int>& nums,vector<vector<int>>&ans, int idx){
         if(idx == nums.size()-1) ans.push_back(nums);
         for(int i=idx; i<nums.size(); i++){
             swap(nums[i],nums[idx]);
-            solve(nums, ans, curr, idx+1);
+            solve(nums, ans,idx+1);
             swap(nums[i],nums[idx]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> curr;
-        solve(nums,ans,curr,0);
+        solve(nums,ans,0);
         return ans;
     }
 };
